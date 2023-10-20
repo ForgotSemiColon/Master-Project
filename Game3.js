4d53058d384ebffaed7932b47f43765f53e425bb
Nairinn <121157422+Nairinn@users.noreply.github.com>
2023-10-19 23:45:29 -0700

    Add files via upload

--- Game3.js
+++ Game3.js
@@ -88,34 +88,3 @@ closePopupButton.addEventListener('click', () => {
     popupMessage.textContent = '';
 });
 
-
-/* animation for floating effect */
-const shape = document.querySelector('.container');
-let x = 0;
-let y = 0;
-let xDirection = 1;
-let yDirection = 1;
-const speed = 2;
-
-function moveShape() {
-    x += xDirection * speed;
-    y += yDirection * speed;
-
-    const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
-
-    // Check for collisions with the screen edges
-    if (x + shape.clientWidth > screenWidth || x < 0) {
-        xDirection *= -1;
-    }
-
-    if (y + shape.clientHeight > screenHeight || y < 0) {
-        yDirection *= -1;
-    }
-
-    shape.style.transform = `translate(${x}px, ${y}px)`;
-
-    requestAnimationFrame(moveShape);
-}
-
-moveShape();
